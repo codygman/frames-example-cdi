a7c87f660609e8a680db1bd7ca31e34239cba802
Cody Goodman <codygman.consulting@gmail.com>
2016-12-27 02:04:55 -0600

    add minimal dependencies and minimal code

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,18 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Frames
+import Frames.CSV
+import Pipes hiding (Proxy)
+import qualified Pipes.Prelude as P
+
+tableTypes "Row"  "data/U.S._Chronic_Disease_Indicators__CDI_.csv"
+
+rows :: Producer Row IO ()
+rows = readTableOpt rowParser "data/U.S._Chronic_Disease_Indicators__CDI_.csv"
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = pipePreview rows 3 cat -- cat is the identity pipe and gets its name from the unix utility "cat"
